e43b43cb26678dd204956b417e3e2d6f1e62dfb2
William Towe <willbur1984@gmail.com>
2012-05-06 18:36:51 -0400

    updated copyright information in all relevant files

--- WabbitCode/main.m
+++ WabbitCode/main.m
@@ -3,7 +3,7 @@
 //  WabbitEdit
 //
 //  Created by William Towe on 12/22/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2011 Revolution Software. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
